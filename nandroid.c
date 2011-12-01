a6b9cff55f548a01fe09042dd28744c0b4ecc89f
xiaolu <server@163.com>
2011-12-01 21:17:03 +0800

    fix backup and restore .android_secure for i9100 stock rom

--- nandroid.c
+++ nandroid.c
@@ -281,14 +281,29 @@ int nandroid_backup(const char* backup_path)
             return ret;
     }
 
-    if (0 != stat("/sdcard/.android_secure", &s))
+    if (0 != ensure_path_mounted("/emmc"))
     {
-        ui_print("No /sdcard/.android_secure found. Skipping backup of applications on external storage.\n");
+        if (0 == ensure_path_mounted("/sdcard") && 0 == stat("/sdcard/.android_secure", &s))
+        {
+            if (0 != (ret = nandroid_backup_partition_extended(backup_path, "/sdcard/.android_secure", 0)))
+                return ret;
+        }
+        else
+        {
+           ui_print("No /sdcard/.android_secure found. Skipping backup of applications on internal storage.\n");
+        }
     }
     else
     {
-        if (0 != (ret = nandroid_backup_partition_extended(backup_path, "/sdcard/.android_secure", 0)))
-            return ret;
+        if (0 == stat("/emmc/.android_secure", &s))
+        {
+           if (0 != (ret = nandroid_backup_partition_extended(backup_path, "/emmc/.android_secure", 0)))
+                return ret;
+        }
+        else
+        {
+           ui_print("No /emmc/.android_secure found. Skipping backup of applications on external storage.\n");
+        }
     }
 
     if (0 != (ret = nandroid_backup_partition_extended(backup_path, "/cache", 0)))
@@ -571,10 +586,16 @@ int nandroid_restore(const char* backup_path, int restore_boot, int restore_syst
         if (restore_data && 0 != (ret = nandroid_restore_partition(backup_path, "/datadata")))
             return ret;
     }
-
-    if (restore_data && 0 != (ret = nandroid_restore_partition_extended(backup_path, "/sdcard/.android_secure", 0)))
-        return ret;
-
+    if (0 != ensure_path_mounted("/emmc"))
+    {
+        if (restore_data && 0 != (ret = nandroid_restore_partition_extended(backup_path, "/sdcard/.android_secure", 0)))
+            return ret;
+    }
+    else
+    {
+        if (restore_data && 0 != (ret = nandroid_restore_partition_extended(backup_path, "/emmc/.android_secure", 0)))
+            return ret;
+    }
     if (restore_cache && 0 != (ret = nandroid_restore_partition_extended(backup_path, "/cache", 0)))
         return ret;
 
